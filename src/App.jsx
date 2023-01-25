653aa4fd6f44ae740e0d4f8d7e150c6e50e3e395
Manu <manuelmmendoza4@gmail.com>
2023-01-25 17:00:54 -0300

    Added routes and components for homepage, navbar and catalogue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "@picocss/pico/css/pico.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Homepage from "./Homepage";
+import ShoppingPage from "./ShoppingPage";
 
 function App() {
   return (
-    <div>
-      <div>Hello World</div>
-      <article>
-        <header>Header</header>
-        Body
-        <footer>Footer</footer>
-      </article>
+    <div className="container-fluid">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<ShoppingPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <h1>Shop</h1>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="/">Homepage</a>
+        </li>
+        <li>
+          <a href="/shop">Shop</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
